cb653b2f39880149788c4d17ef98195d9b46a586
pat saunders <pat@succora.org>
2014-04-30 21:19:11 +1000

    complete authenticationg

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :news
 
   get 'welcome/index'
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ db/migrate/20140430101722_add_user_id_to_news.rb
@@ -0,0 +1,4 @@
+class AddUserIdToNews < ActiveRecord::Migration
+  def change
+  end
+end
