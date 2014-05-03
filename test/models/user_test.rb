93b29307dfa0eee884e726ad3fa6c7bd3f0d669c
pat saunders <pat@succora.org>
2014-05-03 22:22:40 +1000

    Added Views to news and status updates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do 
+
+  	assert !user.save
+
+  	assert !user.errors[:first_name].empty?
+  end
 end
