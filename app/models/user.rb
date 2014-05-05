70edf15ad3866045bbf5726b3d1ed2c8a7a1ecc3
pat saunders <pat@succora.org>
2014-05-05 18:46:52 +1000

    added gravatar profile pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,13 @@ def full_name
 	(first_name + " " + last_name).strip
 end
 
+def gravatar_url
+	stripped_email = email.strip
+	downcased_email = stripped_email.downcase
+	hash = Digest::MD5.hexdigest(downcased_email) 
+
+	"http://gravatar.com/avatar/#{hash}"
+end
 
 
   
